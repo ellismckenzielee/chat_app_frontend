8aad712a972a3a1a11f5fb8b8f409a95abbe1723
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-01 21:17:18 +0000

    add routing to app.js for home and main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Home from "./components/Home";
-
+import Main from "./components/Main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/messenger" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
