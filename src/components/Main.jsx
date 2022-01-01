72f4615e117d1e872092d0721aab1d83c028e0c3
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-01 21:17:51 +0000

    add main component and styling file

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/main.module.css";
+const Main = () => {
+  return (
+    <div className="main">
+      <p>This is the main messenger page</p>
+    </div>
+  );
+};
+export default Main;
