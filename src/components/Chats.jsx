001d3e1d9f7b009f57e8b7b67ad184b99e1c5fa6
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-02 14:03:24 +0000

    add getChatsByUsername and ChatCard to Chats page

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import { getChatsByUsername } from "../utils/utils";
+import ChatCard from "./ChatCard";
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    getChatsByUsername("ellislee").then((chats) => {
+      setChats(chats);
+    });
+  }, []);
   return (
     <div className="chats">
       <p>Chats sidebar</p>
+      {chats.map((chat) => {
+        return <ChatCard key={chat._id} props={chat} />;
+      })}
     </div>
   );
 };
+
+export default Chats;
