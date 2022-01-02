de9d1dcfdf20a6231123b1fc2864be29fc86d2ee
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-02 19:45:31 +0000

    rename chats as chatlist

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from "react";
 import { getChatsByUsername } from "../utils/utils";
+import styles from "../styles/chatList.module.css";
 import ChatCard from "./ChatCard";
-const Chats = () => {
+const ChatList = ({ setChatID }) => {
   const [chats, setChats] = useState([]);
-
+  console.log(chats);
   useEffect(() => {
     getChatsByUsername("ellislee").then((chats) => {
       setChats(chats);
     });
   }, []);
   return (
-    <div className="chats">
+    <div className={styles.chatList}>
       <p>Chats sidebar</p>
       {chats.map((chat) => {
-        return <ChatCard key={chat._id} props={chat} />;
+        return <ChatCard key={chat._id} chat={chat} setChatId={setChatID} />;
       })}
     </div>
   );
 };
 
-export default Chats;
+export default ChatList;
