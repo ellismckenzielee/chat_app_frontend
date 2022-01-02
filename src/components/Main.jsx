74ab75cd2dfef07926631c79e704afd9d4aa2f34
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-02 14:01:35 +0000

    add chats component to main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import styles from "../styles/main.module.css";
+import Chats from "./Chats";
 const Main = () => {
   return (
     <div className="main">
       <p>This is the main messenger page</p>
+      <Chats></Chats>
     </div>
   );
 };
