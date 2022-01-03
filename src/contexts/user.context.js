8ce86038cca5081ce54201446534a2946d1fbc73
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-03 20:58:17 +0000

    create user context

---
+++ src/contexts/user.context.js
@@ -0,0 +1,15 @@
+import react, { useState } from "react";
+const UserContext = react.createContext();
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const isLoggedIn = (user) => {
+    return user.username !== undefined;
+  };
+  const logout = (setUser) => {
+    setUser({});
+    return "Logged out.";
+  };
+  return <UserContext.Provider value={{ user, setUser, isLoggedIn, logout }}> {children}</UserContext.Provider>;
+};
+
+export { UserContext, UserProvider };
