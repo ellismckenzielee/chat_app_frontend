39a67f80237a07da64dee058418a7a157d6b7e78
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-03 11:01:12 +0000

    add socket context and moveRoom function to chatCard

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { SocketContext } from "../contexts/socket.context";
 import styles from "../styles/chatCard.module.css";
-const ChatCard = ({ setChatId, chat }) => {
+const ChatCard = ({ setChatId, chat, chatId }) => {
+  const { socket, moveRoom } = useContext(SocketContext);
   return (
     <div
       onClick={() => {
+        moveRoom();
         setChatId(chat._id);
       }}
       className={styles.chatCard}
