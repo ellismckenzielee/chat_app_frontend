e951e5a0018f8470394dbd0f0626fffe9ed258ec
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-03 20:58:46 +0000

    wrap all components within user provider component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import Home from "./components/Home";
 import Main from "./components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./contexts/user.context";
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/messenger" element={<Main />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/messenger" element={<Main />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </UserProvider>
   );
 }
 
