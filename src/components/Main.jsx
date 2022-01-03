a3b10ede319b8536ae81b2c61426a4eaeec84cda
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-03 10:41:07 +0000

    add socket context to main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,21 @@
 import styles from "../styles/main.module.css";
 import ChatList from "./ChatList";
 import Chat from "./Chat";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { SocketContext, SocketProvider } from "../contexts/socket.context";
 const Main = () => {
   const [chatId, setChatID] = useState("");
   console.log("CHTID", chatId);
+
   return (
     <div className={styles.main}>
-      <ChatList setChatID={setChatID}></ChatList>
-      <Chat chatId={chatId}></Chat>
+      <h2> jibber jabber </h2>
+      <main>
+        <SocketProvider>
+          <ChatList setChatID={setChatID} chatId={chatId}></ChatList>
+          <Chat chatId={chatId}></Chat>
+        </SocketProvider>
+      </main>
     </div>
   );
 };
