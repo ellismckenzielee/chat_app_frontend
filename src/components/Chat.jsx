8075d9c903b6af319a93422e1eed97d3d6dcc1b0
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-03 00:15:54 +0000

    add basic socket structure to Chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,11 +2,24 @@ import { useEffect, useState } from "react";
 import { getMessages } from "../utils/utils";
 import styles from "../styles/chat.module.css";
 import MessageCard from "./MessageCard";
+import { io } from "socket.io-client";
 const Chat = ({ chatId }) => {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
     console.log("use effect");
-    getMessages("ellislee", chatId).then(setMessages);
+    if (chatId !== "") {
+      getMessages("ellislee", chatId).then((messages) => {
+        setMessages(messages);
+        const socket = io("http://localhost:4500", { transports: ["websocket"] });
+        socket.emit("join", chatId);
+        socket.on("message", (msg) => {
+          console.log("MESSAGE!!!", msg);
+        });
+        socket.on("joined", (chatId) => {
+          console.log("RETURNED CHAT ID", chatId);
+        });
+      });
+    }
   }, [chatId]);
   return (
     <div className={styles.chat}>
