9d3e6cab722a0bb5ea75386f6960150904c5d642
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-04 12:08:39 +0000

    add default logged in user for testing

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,7 +1,7 @@
 import react, { useState } from "react";
 const UserContext = react.createContext();
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({ username: "ellislee", name: "ellis" });
   const isLoggedIn = (user) => {
     return user.username !== undefined;
   };
