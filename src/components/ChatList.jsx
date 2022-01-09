e3a93002f2f08b12f780b9b96baac9fee4066f0c
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-09 12:25:30 +0000

    add create chat button to ChatList component

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import { getChatsByUsername } from "../utils/utils";
 import styles from "../styles/chatList.module.css";
 import ChatCard from "./ChatCard";
+import { useNavigate } from "react-router-dom";
 const ChatList = ({ setChatID, chatId }) => {
   const [chats, setChats] = useState([]);
   console.log(chats);
+  const navigate = useNavigate();
   useEffect(() => {
     getChatsByUsername("ellislee").then((chats) => {
       setChats(chats);
@@ -16,6 +18,14 @@ const ChatList = ({ setChatID, chatId }) => {
       {chats.map((chat) => {
         return <ChatCard key={chat._id} chat={chat} chatId={chatId} setChatId={setChatID} />;
       })}
+      <button
+        onClick={() => {
+          navigate("/create");
+        }}
+        className={styles.newChatButton}
+      >
+        New Chat
+      </button>
     </div>
   );
 };
