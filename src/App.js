629e097e6e3274c8e72fbe898d87c189436ac29b
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-09 12:26:16 +0000

    add CreateChat component to App.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./components/Home";
 import Main from "./components/Main";
+import CreateChat from "./components/CreateChat";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/messenger" element={<Main />} />
+            <Route path="/create" element={<CreateChat />} />
           </Routes>
         </BrowserRouter>
       </div>
