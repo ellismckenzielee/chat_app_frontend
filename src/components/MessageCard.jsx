e6457057eaa121b1ce6d0fe0472928ecba0e1ffd
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-10 11:33:47 +0000

    add styling to message card, depending on user or other

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,8 +1,15 @@
 import styles from "../styles/messageCard.module.css";
+import { UserContext } from "../contexts/user.context";
+import { useContext } from "react";
 const MessageCard = ({ message }) => {
+  const { user } = useContext(UserContext);
   return (
-    <div className={styles.messageCard}>
-      <p>{message.message}</p>
+    <div className={user.username === message.sender ? styles.userMessage : styles.otherMessage}>
+      <p>
+        <span className={styles.messageContent}>{message.message}</span>
+        <br />
+        <span className={styles.author}>{message.sender}</span>
+      </p>
     </div>
   );
 };
