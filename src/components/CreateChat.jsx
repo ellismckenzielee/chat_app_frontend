dbf104912cd259ff2da609fc9dae1c4a83eebaa5
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-10 10:01:43 +0000

    add error handling to CreateChat component

--- src/components/CreateChat.jsx
+++ src/components/CreateChat.jsx
@@ -2,27 +2,39 @@ import styles from "../styles/createChat.module.css";
 import { useState, useContext } from "react";
 import { createChat } from "../utils/utils";
 import { UserContext } from "../contexts/user.context";
+import { useNavigate } from "react-router-dom";
 const CreateChat = () => {
   const [recipientUsername, setRecipientUsername] = useState("");
   const { user } = useContext(UserContext);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
   return (
     <div className={styles.createChat}>
-      <p>Create Chat Page</p>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createChat(user.username, recipientUsername).then().catch(console.log);
-        }}
-      >
-        <input
-          onChange={(e) => {
-            setRecipientUsername(e.target.value);
+      <h2 className={styles.title}>Create a New Chat</h2>
+      <div className={styles.main}>
+        <form
+          className={styles.form}
+          onSubmit={(e) => {
+            e.preventDefault();
+            createChat(user.username, recipientUsername)
+              .then(() => {
+                navigate(-1);
+              })
+              .catch(setErrorMessage);
           }}
-          type="text"
-          value={recipientUsername}
-        ></input>
-        <button>Create!</button>
-      </form>
+        >
+          <label> Enter Username </label>
+          <input
+            onChange={(e) => {
+              setRecipientUsername(e.target.value);
+            }}
+            type="text"
+            value={recipientUsername}
+          ></input>
+          {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+          <button>Create!</button>
+        </form>
+      </div>
     </div>
   );
 };
