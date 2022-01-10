a09e9f2a23278b3bcc212d4376b663fdbc7f8e46
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-10 09:05:58 +0000

    add createChat util function to CreateChat component

--- src/components/CreateChat.jsx
+++ src/components/CreateChat.jsx
@@ -1,14 +1,17 @@
 import styles from "../styles/createChat.module.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { createChat } from "../utils/utils";
+import { UserContext } from "../contexts/user.context";
 const CreateChat = () => {
   const [recipientUsername, setRecipientUsername] = useState("");
-
+  const { user } = useContext(UserContext);
   return (
     <div className={styles.createChat}>
       <p>Create Chat Page</p>
       <form
         onSubmit={(e) => {
           e.preventDefault();
+          createChat(user.username, recipientUsername).then().catch(console.log);
         }}
       >
         <input
