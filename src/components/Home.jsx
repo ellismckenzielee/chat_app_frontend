bb1241069ed09371f9cc01846d00f193b3c4e812
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-11 10:11:07 +0000

    add create account button to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,51 @@
 import styles from "../styles/home.module.css";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
   const [username, setUsername] = useState("");
-  useEffect(() => {
-    const socket = io("http://localhost:4500", { transports: ["websocket"] });
-    socket.on("message", () => {
-      console.log("MESSAGE!!!");
-    });
-    axios.get("http://localhost:4500/users").then(console.log);
-  }, []);
+  const navigate = useNavigate();
   return (
     <div className={styles.home}>
-      <h3 className={styles.welcome}>welcome to</h3>
-      <h1>Link</h1>
-      <main>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(username);
-          }}
-          className={styles.loginForm}
-        >
-          <fieldset className={styles.fieldset}>
-            <legend>Sign In!</legend>
-            <label>Username</label>
-            <input
-              value={username}
-              onChange={(e) => {
-                setUsername(e.target.value);
+      <div className={styles.container}>
+        <h3 className={styles.welcome}>welcome to</h3>
+        <h1 className={styles.title}>Link</h1>
+        <main>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(username);
+            }}
+            className={styles.loginForm}
+          >
+            <fieldset className={styles.fieldset}>
+              <legend className={styles.legend}>
+                <h3 className={styles.legendTitle}>Sign In!</h3>
+              </legend>
+              <label>
+                <h3>Username: </h3>
+              </label>
+              <input
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                className={styles.input}
+                type="text"
+              ></input>
+            </fieldset>
+            <button className={styles.submit}>Login</button>
+            <p> Dont' have an account?</p>
+            <button
+              onClick={() => {
+                navigate("/create-user");
               }}
-              className={styles.input}
-              type="text"
-            ></input>
-          </fieldset>
-          <button className={styles.submit}>Login</button>
-        </form>
-      </main>
+              className={styles.create}
+            >
+              Create Account
+            </button>
+          </form>
+        </main>
+      </div>
     </div>
   );
 };
