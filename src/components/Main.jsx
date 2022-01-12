2e2d7f72851c03d3d7a2f57aadfebff445a9f464
Ellis Lee <ellismckenzielee@gmail.com>
2022-01-12 18:18:01 +0000

    remove socketprovidor from main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,16 @@ import ChatList from "./ChatList";
 import Chat from "./Chat";
 import { useContext, useEffect, useState } from "react";
 import { SocketContext, SocketProvider } from "../contexts/socket.context";
+import { UserContext } from "../contexts/user.context";
 const Main = () => {
   const [chatId, setChatID] = useState("");
   console.log("CHTID", chatId);
+  const { user } = useContext(UserContext);
 
   return (
     <div className={styles.main}>
-      <SocketProvider>
-        <ChatList setChatID={setChatID} chatId={chatId}></ChatList>
-        <Chat chatId={chatId}></Chat>
-      </SocketProvider>
+      <ChatList setChatID={setChatID} chatId={chatId}></ChatList>
+      <Chat chatId={chatId}></Chat>
     </div>
   );
 };
